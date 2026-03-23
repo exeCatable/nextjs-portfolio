824c412ffae1ab93c27a463b9a6bf8d4a769054a
exeCatable <50675164+exeCatable@users.noreply.github.com>
2026-03-24 00:25:27 +0100

    Refine personal description in data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -243,9 +243,8 @@ export const aboutMe = [
     description: `
     Hey there! I'm Büsra, a web developer and web designer from Germany.
     Started off as a web designer, but then I made the leap to become a
-    Frontend Developer. off work, you'll catch me sketching—whether
-    it's with a pencil or on a digital canvas. I'm also big on
-    Dungeons and Dragons sessions with my pals, and I like to tinker with my
+    Frontend Developer. Off work, you'll catch me sketching whether
+    it's with a pencil or on a digital canvas. I like to tinker with my
     own coding projects in my free time.`,
   },
 ] as const;
